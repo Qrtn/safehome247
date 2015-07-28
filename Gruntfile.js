60ac410bfaf567711237a9a13d8d76f77f220622
Jeffrey Tang <jeffrey.tang@yandex.com>
2015-07-28 01:38:15 -0500

    Tidy up management stuff on the front-end

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,50 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     concurrent: {
-      tasks: ['nodemon', 'watch'],
+      tasks: ['nodemon:web', 'nodemon:alert', 'nodemon:logger', 'watch'],
       options: {
         logConcurrentOutput: true
       }
     },
     nodemon: {
-      script: 'index.js'
+      web: {
+        script: 'index.js'
+      },
+      alert: {
+        script: 'alert.js'
+      },
+      logger: {
+        script: 'logger.js'
+      }
     },
     watch: {
       files: ['index.js', 'views/**/*', 'public/**/*'],
       options: {
         livereload: true
+      }
+    },
+
+    bower_concat: {
+      all: {
+        dest: 'public/js/bower.js',
+        cssDest: 'public/css/bower.css',
+        mainFiles: {
+          'bootstrap': ['dist/css/bootstrap.css', 'dist/js/bootstrap.js']
+        }
+      }
+    },
+    uglify: {
+      bower: {
+        files: {
+          'public/js/bower.min.js': 'public/js/bower.js'
+        }
+      }
+    },
+    cssmin: {
+      bower: {
+        files: {
+          'public/css/bower.min.css': 'public/css/bower.css'
+        }
       }
     }
   });
@@ -22,6 +54,11 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['concurrent']);
+  grunt.loadNpmTasks('grunt-bower-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('serve', ['concurrent']);
+  grunt.registerTask('buildbower', ['bower_concat', 'uglify:bower', 'cssmin:bower']);
 
 };
